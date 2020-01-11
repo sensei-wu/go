11027b9b8abfd5113cb8650a9cb1507b2b94ebdc
Zenifer Cheruveettil <zen_cv@yahoo.com>
2020-01-11 13:08:31 +0100

    time service

---
+++ cmd/services/time-service/print_time.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("0.0.0.0:9090", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Time now is: %s\n", t.Format(time.RFC850))))
+	w.Write([]byte(fmt.Sprintf("Hostname: %s\n", hostname)))
+}
