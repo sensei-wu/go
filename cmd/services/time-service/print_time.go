cdeaf2d7cbe0c67c8aa593404b5b3c0281bfa58a
Zenifer Cheruveettil <zen_cv@yahoo.com>
2020-03-17 23:08:21 +0100

    added useful info

--- cmd/services/time-service/print_time.go
+++ cmd/services/time-service/print_time.go
@@ -9,12 +9,14 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting server at 0.0.0.0:9090")
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("0.0.0.0:9090", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
+	v := "v1"
 
 	hostname, err := os.Hostname()
 	if err != nil {
@@ -23,4 +25,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte(fmt.Sprintf("Time now is: %s\n", t.Format(time.RFC850))))
 	w.Write([]byte(fmt.Sprintf("Hostname: %s\n", hostname)))
+	w.Write([]byte(fmt.Sprintf("Version: %s\n", v)))
 }
