ecb94c717fff44e609133088b733a5ec9915683e
Zenifer Cheruveettil <zen_cv@yahoo.com>
2020-07-31 19:27:22 +0200

    sample

---
+++ cmd/strings/samples.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	jaba := "A hello world"
+
+	fmt.Println(len(jaba))
+
+	fmt.Println(jaba[1])
+
+	fmt.Println(jaba[2:])
+
+	fmt.Println(jaba[0:1])
+
+	
+}
